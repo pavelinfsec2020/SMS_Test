2c4318a437a754cfae3a1d7b4f184247db8f4710
infsec <infsec@HOME-PC>
2026-03-04 18:01:41 +0300

    Added empty HttpClient lib, added dish and menu models with json

---
+++ HttpClient/Models/MenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace HttpClient.Models
+{
+    public class MenuItem
+    {
+        [JsonPropertyName("Id")]
+        public string Id { get; set; } = string.Empty;
+
+        [JsonPropertyName("Article")]
+        public string Article { get; set; } = string.Empty;
+
+        [JsonPropertyName("Name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("Price")]
+        public decimal Price { get; set; }
+
+        [JsonPropertyName("IsWeighted")]
+        public bool IsWeighted { get; set; }
+
+        [JsonPropertyName("FullPath")]
+        public string FullPath { get; set; } = string.Empty;
+
+        [JsonPropertyName("Barcodes")]
+        public List<string> Barcodes { get; set; } = new();
+    }
+}
---
+++ HttpClient/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace HttpClient.Models
+{
+    public class Order
+    {
+        [JsonPropertyName("OrderId")]
+        public string OrderId { get; set; } = string.Empty;
+
+        [JsonPropertyName("MenuItems")]
+        public List<OrderItem> MenuItems { get; set; } = new();
+    }
+
+    public class OrderItem
+    {
+        [JsonPropertyName("Id")]
+        public string Id { get; set; } = string.Empty;
+
+        [JsonPropertyName("Quantity")]
+        public string Quantity { get; set; } = string.Empty;
+    }
+}
