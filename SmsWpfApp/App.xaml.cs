31473db5f780fdb2fc937ce18ec0e2a15b50287d
infsec <infsec@HOME-PC>
2026-03-05 10:13:54 +0300

    Created empty wpf project, added some packages for logging

---
+++ SmsWpfApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SmsWpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
